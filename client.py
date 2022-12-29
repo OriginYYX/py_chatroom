235ecd2921cd4dc1f339efbd1eefcdab858d397c
OriginYYX <oringin@icloud.com>
2022-12-29 22:52:56 +0800

    basic functions

--- client.py
+++ client.py
@@ -1,2 +1,34 @@
-import threading
 import socket
+import threading
+import sys
+
+HOST = '127.0.0.1'
+PORT = 10239
+name = input("昵称: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+def ServerThread():
+    while True:
+        try:
+            msg = client.recv(1024).decode('utf-8')  
+            if msg == '昵称：':                           
+                client.send(name.encode('utf-8'))    
+            else:
+                print(msg)
+        except:
+            print("An error occured!")
+            client.close()
+            break
+
+def write():
+    while True:  
+        msg = '{}: {}'.format(name, input(''))
+        client.send(msg.encode('utf-8'))
+
+server_thread = threading.Thread(target=ServerThread)
+server_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start() 
--- server.py
+++ server.py
@@ -1,2 +1,52 @@
-import threading
 import socket
+import threading
+import sys
+
+HOST = '127.0.0.1'
+PORT = 10239
+
+clients = [] 
+names = []
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         
+server.bind((HOST, PORT))                                          
+server.listen()
+
+def PublishMsg(msg):
+    for c in clients:
+        c.send(msg)
+
+def ClientThread(client):
+    while True:
+        try:
+            msg = client.recv(1024)                      
+            PublishMsg(msg)          
+        except:
+            index = clients.index(client)        
+            clients.remove(client)       
+            client.close()
+            name = names[index]
+            PublishMsg('{} left!'.format(name).encode('utf-8'))  
+            names.remove(name)
+            break
+
+def LoopListenAccept():
+    while True:
+        client, address = server.accept()                        
+        print("Connected with {}".format(str(address)))           
+
+        client.send('昵称：'.encode('utf-8'))                       
+        name = client.recv(1024).decode('utf-8')                
+        names.append(name)
+        clients.append(client)
+
+        print("Name is {}".format(name))
+
+        PublishMsg("{} 加入了!".format(name).encode('utf-8'))  
+        client.send('Connected to server!'.encode('utf-8'))        
+
+        thread = threading.Thread(target=ClientThread, args=(client,))   
+        thread.start()
+
+print("Server start")
+LoopListenAccept()
